db97773768e10a1bb9ec01d1c37506c0da3e03a1
Philippe Verdy <1387035+verdy-p@users.noreply.github.com>
2023-11-24 00:48:05 +0100

    Update prototype-1.6.1_rc2.js

--- src/main/webapp/js/prototype-1.6.1_rc2.js
+++ src/main/webapp/js/prototype-1.6.1_rc2.js
@@ -676,13 +676,13 @@ String.prototype.escapeHTML.div.appendChild(String.prototype.escapeHTML.text);
 
 if ('<\n>'.escapeHTML() !== '&lt;\n&gt;') {
   String.prototype.escapeHTML = function() {
-    return this.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    return this.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   }
 }
 
 if ('&lt;\n&gt;'.unescapeHTML() !== '<\n>') {
   String.prototype.unescapeHTML = function() {
-    return this.stripTags().replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&');
+    return this.stripTags().replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');
   }
 }
 var Template = Class.create({
